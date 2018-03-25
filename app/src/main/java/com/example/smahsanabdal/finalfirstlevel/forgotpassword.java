2f85a98669f8d320e2e43e99849f4aa0febf212c
saiyedabdal <smahsanabdal@gmail.com>
2018-03-25 23:24:52 +0530

    First GIT HUb Push

---
+++ app/src/main/java/com/example/smahsanabdal/finalfirstlevel/forgotpassword.java
@@ -0,0 +1,13 @@
+package com.example.smahsanabdal.finalfirstlevel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class forgotpassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fp);
+    }
+}
