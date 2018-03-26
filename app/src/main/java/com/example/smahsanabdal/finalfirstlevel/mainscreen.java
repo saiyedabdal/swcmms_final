d41428b2b5685dd8e83622ce81d921e1752d2055
rockeypandit <rockey.pandit98@gmail.com>
2018-03-26 05:51:41 +0530

    activity changes on click

---
+++ app/src/main/java/com/example/smahsanabdal/finalfirstlevel/mainscreen.java
@@ -0,0 +1,13 @@
+package com.example.smahsanabdal.finalfirstlevel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class mainscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainscreen);
+    }
+}
